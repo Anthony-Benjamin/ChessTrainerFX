b0e217d775db41b3e5995e9e8e400ff8d2d9ea9e
Anthony-Benjamin <anthony_benjamin@rocketmail.com>
2025-08-08 15:25:49 +0200

    Replaced unicode characters to images in setupmode. Who's turn is now working

--- src/main/java/application/chesstrainerfx/Controller.java
+++ src/main/java/application/chesstrainerfx/Controller.java
@@ -6,6 +6,8 @@ public class Controller {
     private Position source, target;
     private SquareView from, to;
     private PieceModel piece;
+    private boolean isWhiteTurn = true;
+    private boolean correctPieceSelected = false;
 
     public boolean isSetupMode() {
         return setupMode;
@@ -39,72 +41,100 @@ public class Controller {
             view.update(); // herteken GUI
             return;
         }
-
-        if (counter == 0) {
-            from = view;
-            from.setSeletedSource();
-            source = model.getPosition();
-            System.out.println("Source: " + source + model.getPiece());
-            piece = model.getPiece();
-            counter = 1;
-        } else {
-            // Tweede klik
-            to = view;
-            to.setSelectedTarget();
-            target = model.getPosition();
-            System.out.println("Target: " + target);
-
-            boolean result = MoveValidator.isValidMove(board, piece, source, target);
-
-
-            System.out.println(result);
-            if (result) {
-                if (piece.getType() == PieceType.PAWN) {
-                    int dx = target.getColumn() - source.getColumn();
-                    int dy = target.getRow() - source.getRow();
-                    int direction = piece.getColor() == PieceColor.WHITE ? -1 : 1;
-
-                    //En passant
-                    if (Math.abs(dx) == 1 && dy == direction && board.getSquare(target).getPiece() == null) {
-                        Position captured = new Position(source.getRow(), target.getColumn());
-                        board.getSquare(captured).setPiece(null);
-                        // GUI update
-                        view.update();
-
-                    }
-                    // Dubbele stap
-                    if (Math.abs(dy) == 2) {
-                        board.setLastDoubleStepPawnPosition(target);
-                    } else {
-                        board.setLastDoubleStepPawnPosition(null);
-                    }
-
-                }
-                board.movePiece(source, target);
-
-                // Promotie
-                if (piece.getType() == PieceType.PAWN && (target.getRow() == 0 || target.getRow() == 7)) {
-                    PieceModel promoted = new PieceModel(PieceType.QUEEN, piece.getColor());
-                    board.getSquare(target).setPiece(promoted);
-                    view.update();
-                }
+        PieceColor color;
+        if(isWhiteTurn){
+            color = PieceColor.WHITE;
+        }else{
+            color = PieceColor.BLACK;
+        }
+        PieceModel p = null;
+        if(counter == 0){
+            p = model.getPiece();
+            if(p != null && p.getColor().equals(color)){
+                correctPieceSelected = true;
             }
-
-
-            if (from != null && to != null) {
-                from.removeSelection();
-                to.removeSelection();
-            }
-
-            // reset
-            source = null;
-            target = null;
-            from = null;
-            to = null;
-            counter = 0;
         }
 
+        System.out.println("piece? " + p);
+        if(correctPieceSelected) {
 
+
+            if (counter == 0) {
+
+                from = view;
+                from.setSeletedSource();
+                source = model.getPosition();
+                System.out.println("Source: " + source + model.getPiece());
+                piece = model.getPiece();
+                counter = 1;
+                System.out.println(isWhiteTurn);
+                System.out.println(piece.getColor());
+
+            } else {
+                // Tweede klik
+                to = view;
+                to.setSelectedTarget();
+                target = model.getPosition();
+                System.out.println("Target: " + target);
+
+                boolean result = MoveValidator.isValidMove(board, piece, source, target);
+
+
+                System.out.println(result);
+                if (result) {
+                    if (piece.getType() == PieceType.PAWN) {
+                        int dx = target.getColumn() - source.getColumn();
+                        int dy = target.getRow() - source.getRow();
+                        int direction = piece.getColor() == PieceColor.WHITE ? -1 : 1;
+
+                        //En passant
+                        if (Math.abs(dx) == 1 && dy == direction && board.getSquare(target).getPiece() == null) {
+                            Position captured = new Position(source.getRow(), target.getColumn());
+                            board.getSquare(captured).setPiece(null);
+                            // GUI update
+                            view.update();
+
+                        }
+                        // Dubbele stap
+                        if (Math.abs(dy) == 2) {
+                            board.setLastDoubleStepPawnPosition(target);
+                        } else {
+                            board.setLastDoubleStepPawnPosition(null);
+                        }
+
+                    }
+                    board.movePiece(source, target);
+                    toggleTurn();
+
+                    // Promotie
+                    if (piece.getType() == PieceType.PAWN && (target.getRow() == 0 || target.getRow() == 7)) {
+                        PieceModel promoted = new PieceModel(PieceType.QUEEN, piece.getColor());
+                        board.getSquare(target).setPiece(promoted);
+                        view.update();
+                    }
+                }
+
+
+                if (from != null && to != null) {
+                    from.removeSelection();
+                    to.removeSelection();
+                }
+
+                // reset
+                source = null;
+                target = null;
+                from = null;
+                to = null;
+                counter = 0;
+                correctPieceSelected = false;
+            }
+//            if(counter == 1) {
+//                correctPieceSelected = false;
+//            }
+        }
+    }
+    public void toggleTurn(){
+        this.isWhiteTurn = !isWhiteTurn;
     }
 }
 
